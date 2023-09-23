e0953b550e9e4df7e36dd85ab176ec11c7034b0c
김우빈 <kwb020312@naver.com>
2023-09-23 21:47:50 +0900

    Feat: Apply Gravity for Car

--- src/Car.jsx
+++ src/Car.jsx
@@ -1,15 +1,39 @@
+import { useBox } from "@react-three/cannon";
 import { useLoader } from "@react-three/fiber";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/gltfloader";
 
 const Car = () => {
   let mesh = useLoader(GLTFLoader, "/models/car.glb").scene;
 
+  const position = [-1.5, 0.5, 3];
+  const width = 0.15;
+  const height = 0.07;
+  const front = 0.15;
+  const wheelRadius = 0.05;
+
+  const chassisBodyArgs = [width, height, front * 2];
+  // 물리 엔진에 적용되게 하는 메서드
+  const [chassisBody, chassisApi] = useBox(
+    () => ({
+      args: chassisBodyArgs,
+      mass: 150,
+      position,
+    }),
+    useRef(null)
+  );
+
   useEffect(() => {
     mesh.scale.set(0.0012, 0.0012, 0.0012);
     mesh.children[0].position.set(-365, -18, -67);
   }, [mesh]);
-  return <primitive object={mesh} rotation-y={Math.PI} />;
+  return (
+    <mesh ref={chassisBody}>
+      <meshBasicMaterial transparent={true} opacity={0.3} />
+      <boxGeometry args={chassisBodyArgs} />
+      {/* <primitive object={mesh} rotation-y={Math.PI} /> */}
+    </mesh>
+  );
 };
 
 export default Car;
