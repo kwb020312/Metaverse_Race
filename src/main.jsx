9220e9a56d7d9d559f088a1b8c41dbcdd046497c
김우빈 <kwb020312@naver.com>
2023-09-24 18:15:21 +0900

    Feat: Add Tooltip

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,27 @@ import { Physics } from "@react-three/cannon";
 import Scene from "./Scene";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Canvas>
-    <Physics broadphase="SAP" gravity={[0, -2.6, 0]}>
-      <Scene />
-    </Physics>
-  </Canvas>
+  <>
+    <Canvas>
+      <Physics broadphase="SAP" gravity={[0, -2.6, 0]}>
+        <Scene />
+      </Physics>
+    </Canvas>
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        right: 0,
+        backgroundColor: "rgba(255, 255, 255, 0.5)",
+      }}
+    >
+      이동: w,a,s,d
+      <br />
+      회전: 방향키
+      <br />
+      다시시작: r
+      <br />
+      시점변환: k
+    </div>
+  </>
 );
