36a3d982fb94410599c75a255b3cce52c898823f
김우빈 <kwb020312@naver.com>
2023-09-24 11:57:04 +0900

    Feat: Add Restart & Car Reverse when it fall down

--- src/useControls.jsx
+++ src/useControls.jsx
@@ -55,6 +55,21 @@ export const useControls = (vehicleApi, chassisApi) => {
         vehicleApi.setSteeringValue(0, i);
       }
     }
+
+    if (controls.arrowdown)
+      chassisApi.applyLocalImpulse([0, -5, 0], [0, 0, +1]);
+    if (controls.arrowup) chassisApi.applyLocalImpulse([0, -5, 0], [0, 0, -1]);
+    if (controls.arrowleft)
+      chassisApi.applyLocalImpulse([0, -5, 0], [-0.5, 0, 0]);
+    if (controls.arrowright)
+      chassisApi.applyLocalImpulse([0, -5, 0], [0.5, 0, 0]);
+
+    if (controls.r) {
+      chassisApi.position.set(-1.5, 0.5, 3);
+      chassisApi.velocity.set(0, 0, 0);
+      chassisApi.angularVelocity.set(0, 0, 0);
+      chassisApi.rotation.set(0, 0, 0);
+    }
   }, [controls, vehicleApi, chassisApi]);
 
   return controls;
