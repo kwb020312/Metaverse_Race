c8bb2b14b0a0f3ccfbe338a650875c599f92e92b
김우빈 <kwb020312@naver.com>
2023-09-24 10:53:40 +0900

    Feat: Add Ramp With TriMesh Physics

---
+++ src/Ramp.jsx
@@ -0,0 +1,24 @@
+import { useTrimesh } from "@react-three/cannon";
+import { useLoader } from "@react-three/fiber";
+import { useRef } from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/gltfloader";
+
+const Ramp = () => {
+  const result = useLoader(GLTFLoader, "./models/ramp.glb");
+
+  const geometry = result.scene.children[0].geometry;
+
+  const vertices = geometry.attributes.position.array;
+  const indices = geometry.index.array;
+
+  const [ref] = useTrimesh(
+    () => ({
+      args: [vertices, indices],
+      mass: 0,
+      type: "Static",
+    }),
+    useRef(null)
+  );
+};
+
+export default Ramp;
--- src/Track.jsx
+++ src/Track.jsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { TextureLoader } from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/gltfloader";
 import { ColliderBox } from "./ColliderBox";
+import Ramp from "./Ramp";
 
 const Track = () => {
   const result = useLoader(GLTFLoader, "/models/track.glb");
@@ -54,6 +55,8 @@ const Track = () => {
       <ColliderBox position={[-4.15, 0, -0.67]} scale={[0.1, 0.5, 0.1]} />
       <ColliderBox position={[-4.9, 0, -0.58]} scale={[0.1, 0.5, 0.1]} />
       <ColliderBox position={[-0.3, 0, 1]} scale={[0.1, 0.5, 0.1]} />
+
+      <Ramp />
     </>
   );
 };
